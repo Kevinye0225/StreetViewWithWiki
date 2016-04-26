98996dece88b7958a94aee36d9cff8dd6d4822f6
Kevinye0225 <yeyfly@hotmail.com>
2016-04-26 14:49:15 -0400

    weather added

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $weatherElem = $('#weather-links');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    $weatherElem.text("");
 
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
@@ -21,9 +23,20 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ cityStr +'&units=metric&appid=fb6f84449a540ac2c62dd80a8e292b2b';
+    $.getJSON(weatherUrl, function(data){
+        // $weatherElem.text('message:' + )
+        weather = data.main.temp;
+        $weatherElem.append('<p> The temperature is ' + weather + 'C </p>');
+        // $weatherElem.append('<p>' + weather + '</p>');
+    }).error(function(e){
+        $weatherElem.text("There is an error oops");
+    });
+
+
 
     // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633';
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
