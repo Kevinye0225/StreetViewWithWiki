27a84c6cebe53b7f633f66a7f02d1ddcbea3553a
Kevinye0225 <yeyfly@hotmail.com>
2016-05-02 19:39:33 -0400

    credential removed

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ cityStr +'&units=metric&appid=fb6f84449a540ac2c62dd80a8e292b2b';
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+ cityStr +'&units=metric&appid=APIKEY';
     $.getJSON(weatherUrl, function(data){
         // $weatherElem.text('message:' + )
         weather = data.main.temp;
@@ -36,7 +36,7 @@ function loadData() {
 
 
     // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633';
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=APIKEY';
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
